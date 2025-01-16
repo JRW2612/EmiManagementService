c06c6d80ee06ead84c9f3b1b987db14edf274b5b
JRW2612 <jayantwaghale.jw@gmail.com>
2025-01-16 11:48:31 +0530

    project update

--- EmiManager.Repository/obj/Debug/net6.0/EmiManager.Repository.AssemblyInfo.cs
+++ EmiManager.Repository/obj/Debug/net6.0/EmiManager.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmiManager.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b33d1b769938beeb444fbc88efbbf096e2fca5aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2b181c14b2acdff0664ea5203c0d5db68a2a063")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmiManager.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmiManager.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
